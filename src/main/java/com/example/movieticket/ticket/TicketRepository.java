8737ea32e5e524260ab6d36ce0c1c6e1cb179cba
nadine2000 <107400308+nadine2000@users.noreply.github.com>
2026-03-13 02:21:30 +0200

    Add comment about UUID in TicketRepository
    Added a comment indicating that the UUID is not long.

--- src/main/java/com/example/movieticket/ticket/TicketRepository.java
+++ src/main/java/com/example/movieticket/ticket/TicketRepository.java
@@ -2,7 +2,7 @@ package com.example.movieticket.ticket;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// not long uuid
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     boolean existsByShowtimeIdAndSeatNumber(Long showtimeId, Integer seatNumber);
 }
